0ae4534a1417d4cbd34c4fdb73f3546a83a4879c
Enric Balletbo i Serra <eballetbo@iseebcn.com>
2013-07-15 13:48:14 +0200

    suites/example.py: An example class implementation of a test suite.
    Simple Test Suite that use the Test Case from network module.
    
    Signed-off-by: Enric Balletbo i Serra <eballetbo@iseebcn.com>

---
+++ igep_qa/suites/example.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+"""
+For every test suite we create an instance of TestSuite and add test case
+instances. When all tests have been added, the suite can be passed to a test
+runner, such as TextTestRunner. It will run the individual test cases in the
+order in which they were added, aggregating the results.
+
+An example class implementation of a test suite:
+
+.. code:: python
+
+    import unittest
+    from tests import network
+
+    def TestSuiteMyExample():
+        suite = unittest.TestSuite()
+        suite.addTest(network.TestNetwork("test_ping_host", "127.0.0.1", "127.0.0.1", "eth0"))
+        return suite
+
+The main program just runs the test suite, the output format depends on the
+Test Runner. Here we'll launch the test suite twice. The first one, uses the
+popular TextTestRunner from unittest module. The second one, uses a very simple
+Test Runner that only prints a '.' if the test case has been successed, and an
+'F' if the test case has been failed.
+
+.. code:: python
+
+    from runners import lightly
+
+    if __name__ == '__main__':
+        # Output Using TextTestRunner from unittest module"
+        unittest.TextTestRunner(verbosity=2).run(TestSuiteMyExample())
+        # Output using LightlyTestRunner from lightly module"
+        suite =  lightly.LightlyTestRunner()
+        suite.run(TestSuiteMyExample())
+
+"""
+
+import unittest
+from igep_qa.tests import network
+
+def TestSuiteMyExample():
+    """ An example class implementation of a test suite.
+
+    What is tested?
+        - Network : Ping the IP address of a remote host.
+
+    """
+    suite = unittest.TestSuite()
+    suite.addTest(network.TestNetwork("test_ping_host", "127.0.0.1", "127.0.0.1", "eth0"))
+    return suite
+
+from igep_qa.runners import lightly
+
+if __name__ == '__main__':
+    # Output Using TextTestRunner from unittest module"
+    unittest.TextTestRunner(verbosity=2).run(TestSuiteMyExample())
+    # Output using LightlyTestRunner from lightly module"
+    suite =  lightly.LightlyTestRunner()
+    suite.run(TestSuiteMyExample())
