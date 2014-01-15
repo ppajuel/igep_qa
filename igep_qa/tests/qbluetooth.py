9d7ad10c6cf822b097da88897724680e062dae48
Enric Balletbo i Serra <eballetbo@iseebcn.com>
2014-01-15 18:14:05 +0100

    tests/qbluetooth: Add test to attach serial devices via UART HCI
    Attach to serial device connected to <port> using hciattach command.
    
    Signed-off-by: Enric Balletbo i Serra <eballetbo@iseebcn.com>

--- igep_qa/tests/qbluetooth.py
+++ igep_qa/tests/qbluetooth.py
@@ -13,14 +13,18 @@ class TestBluetooth(unittest.TestCase):
 
     Keyword arguments:
         - port: Serial device. E.g. /dev/ttyS0, /dev/ttyUSB0
+        - options: Use this parameter to pass other supported options to the
+                   hciattach command. To see what options are available just
+                   type hciattach without arguments in a terminal.
 
     Prerequisite commands:
         - bccmd
 
     """
-    def __init__(self, testname, port):
+    def __init__(self, testname, port, options = ''):
         super(TestBluetooth, self).__init__(testname)
         self.port = port
+        self.options = options
 
     def test_get_chip_revision(self):
         """ Test Bluetooth : Get chip revision
@@ -35,6 +39,17 @@ class TestBluetooth(unittest.TestCase):
                                           "" % self.port)
         self.failUnless(retval[0]==0, "error: Failed to get chip revision")
 
+    def test_attach_uart_hci(self):
+        """ Test Bluetooth : Attach serial devices via UART HCI to BlueZ stack
+
+        Type: Functional
+
+        Description:
+            Attach to serial device connected to <port> using hciattach command.
+
+        """
+        retval = commands.getstatusoutput("hciattach %s %s" % (self.port, self.options))
+        self.failUnless(retval[0]==0, "error: Failed to attach to bluetooth ")
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
