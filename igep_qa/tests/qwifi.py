275ccafe8913b523204e3f9adb69c5807152d944
Enric Balletbo i Serra <eballetbo@iseebcn.com>
2014-04-14 10:34:06 +0200

    tests/qwifi: Add test adhoc mode with wep encryption.
    Signed-off-by: Enric Balletbo i Serra <eballetbo@iseebcn.com>

--- igep_qa/tests/qwifi.py
+++ igep_qa/tests/qwifi.py
@@ -20,6 +20,7 @@ class TestWiFi(unittest.TestCase):
         - testname:  The name of the test to be executed.
         - essid: The EESID to be connected.
         - serverip : The remote IP address.
+        - ipaddr : The local IP address (some tests acquire the ip via dhcp).
 
     Prerequisite commands:
         - iwconfig
@@ -28,14 +29,11 @@ class TestWiFi(unittest.TestCase):
         - ping
 
     """
-    def __init__(self, testname, essid, serverip):
+    def __init__(self, testname, essid, serverip, ipaddr=""):
         super(TestWiFi, self).__init__(testname)
         self.serverip = serverip
         self.essid = essid
-
-    def setUp(self):
-        # Ensure that interface is down
-        commands.getstatusoutput("ifconfig wlan0 down")
+        self.ipaddr = ipaddr
 
     def test_ping_host(self):
         """  Test WiFi : Ping the IP address of a remote host
@@ -49,6 +47,8 @@ class TestWiFi(unittest.TestCase):
             configure a remote dhcp server.
 
         """
+        retval = commands.getstatusoutput("ip link set wlan0 down")
+        self.failUnless(retval[0] == 0, "failed: Can't down interface wlan0")
         retval = commands.getstatusoutput("iwconfig wlan0 essid %s channel 1"
                                           "" % self.essid)
         self.failUnless(retval[0] == 0, "failed: No wlan0 interface found.")
@@ -70,6 +70,8 @@ class TestWiFi(unittest.TestCase):
             a WiFi ESSID, after that set down the 'interface'.
 
         """
+        retval = commands.getstatusoutput("ip link set wlan0 down")
+        self.failUnless(retval[0] == 0, "failed: Can't down interface wlan0")
         retval = commands.getstatusoutput("ip link set wlan0 up")
         self.failUnless(retval[0] == 0, "failed: No wlan0 interface found.")
         retval = commands.getstatusoutput("iw dev wlan0 scan")
@@ -77,6 +79,41 @@ class TestWiFi(unittest.TestCase):
         self.failUnless(self.essid in retval[1], "failed: ESSID %s not found." % self.essid)
         retval = commands.getstatusoutput("ip link set wlan0 down")
         self.failUnless(retval[0] == 0, "failed: Can't down the interface.")
+
+    def test_adhoc_with_wep_encryption(self):
+        """  Test WiFi : Ping the IP address of a remote host (adhoc+wep)
+
+        Type: Functional
+
+        Description:
+            The test connects to server in ad-hoc mode with WEP encryption,
+            then tries to send a echo request ("ping") that is expected to
+            be received back in an echo reply. You must configure a remote dhcp server.
+
+        .. warning::
+
+            AT THE MOMENT THIS TEST ONLY WORKS THE FIRST TIME THAT RUNS.
+            There is a bug in kernel (2.6.37-8) that causes the wlan connection
+            doesn't work setting down and up the interface if the interface was
+            already up before, as a workaround to do this test don't set down
+            the interface. Please make sure the interface was never set up,
+            otherwise the test may fail.
+
+        """
+        # retval = commands.getstatusoutput("ip link set wlan0 down")
+        # self.failUnless(retval[0] == 0, "failed: Can't down interface wlan0")
+        retval = commands.getstatusoutput("ip link set wlan0 up")
+        self.failUnless(retval[0] == 0, "failed: No wlan0 interface found.")
+        retval = commands.getstatusoutput("iw wlan0 set type ibss")
+        self.failUnless(retval[0] == 0, "failed: %s" % retval[1])
+        commands.getstatusoutput("ifconfig wlan0 %s" % self.ipaddr)
+        # For now there are some fixed parameters like channel and key
+        retval = commands.getstatusoutput("iw wlan0 ibss join %s 2422 key d:0:a2PheIrWs23-f" % self.essid)
+        self.failUnless(retval[0] == 0, "failed: %s" % retval[1])
+        retval = commands.getstatusoutput("ping -I wlan0 -c 5 -s 8096 %s" % self.serverip)
+        self.failUnless(retval[0] == 0, "failed: Pinging to %s" % self.serverip)
+        # retval = commands.getstatusoutput("ip link set wlan0 down")
+        # self.failUnless(retval[0] == 0, "failed: Can't down interface wlan0")
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
