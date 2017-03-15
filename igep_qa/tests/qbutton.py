e8a44a86aeff5f89fee2aa555b05cdf6cc6512f7
Pau Pajuelo <ppajuelo@iseebcn.com>
2017-03-15 17:48:43 +0100

    tests/qbutton: add test_button test function
    Signed-off-by: Pau Pajuelo <ppajuelo@iseebcn.com>

--- igep_qa/tests/qbutton.py
+++ igep_qa/tests/qbutton.py
@@ -21,7 +21,10 @@ class TestButton(unittest.TestCase):
     def __init__(self, testname, gpio_in):
         super(TestButton, self).__init__(testname)
         self.gpio_in = QGpio(gpio_in)
-        self._testMethodDoc = "\033[33m PLEASE, PRESS USER BUTTON (S1200) TO TEST IT. THEN, IF THE FOLLOWING IMAGE PATTERN IS CORRECT PRESS USER BUTTON AGAIN OR WAIT 30 SECONDS \033\n[0m"
+        if testname == "test_button_fbtest":
+            self._testMethodDoc = "\033[33m PLEASE, PRESS USER BUTTON (S1200) TO TEST IT. THEN, IF THE FOLLOWING IMAGE PATTERN IS CORRECT PRESS USER BUTTON AGAIN OR WAIT 30 SECONDS.\033\n[0m"
+        if testname == "test_button":
+            self._testMethodDoc = "\033[33m PLEASE, PRESS USER BUTTON (S1200) TO TEST IT OR WAIT 30 SECONDS.\033\n[0m"
 
     def test_button_fbtest(self):
         """ Test Button Fbtest : Read User button action and display fb-test pattern
@@ -29,7 +32,7 @@ class TestButton(unittest.TestCase):
         Type: Functional
 
         Description:
-            - This test needs the interaction with the user. First of all, is 
+            - This test needs the interaction with the user. First of all, is
               necessary to pulse the user button to display fb-test pattern. Once user
               has validated that fb-test pattern is correct, press again user button to
               finish and save test results.
@@ -77,6 +80,47 @@ class TestButton(unittest.TestCase):
         if not events:
             exit_commands()
             self.fail("Error timeout, unable to get second button press")
+
+        exit_commands()
+
+    def test_button(self):
+        """ Test Button : Read User button action
+
+        Type: Functional
+
+        Description:
+            - This test needs the interaction with the user. It is
+              necessary to pulse the user button to PASS TEST.
+
+        """
+
+        def exit_commands():
+            self._testMethodDoc = "Test Button : Read User button action"
+
+            retval = commands.getstatusoutput("echo '\033[37mTest Button : Read User button action:  \033' > /dev/ttyO0")
+            self.failUnless(retval[0] == 0, "failed: Can't execute 'echo'")
+
+        self.gpio_in.set_direction("in")
+        retval = self.gpio_in.get_direction()
+        self.failUnless(retval == 'in\n',
+            "Error: Expected value 'in' and readed %s" % retval)
+
+        self.gpio_in.set_edge("falling")
+        retval = self.gpio_in.get_edge()
+        self.failUnless(retval == 'falling\n',
+            "Error: Expected value 'falling' and readed %s" % retval)
+
+        f = open(os.path.join(self.gpio_in.sysfs, 'value'), 'r')
+
+        po = select.poll()
+        po.register(f, select.POLLPRI)
+
+        f.seek(0)
+        state_last = f.read()
+        events = po.poll(30000)
+        if not events:
+            exit_commands()
+            self.fail("Error timeout, unable to get button press")
 
         exit_commands()
 
