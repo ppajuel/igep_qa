1a2bdc2c89397555119437af351debafb72a6600
Pau Pajuelo <ppajuelo@iseebcn.com>
2017-07-11 17:09:57 +0200

    tests/qaudio: Add Test Case test_audio_workaround_loopback.
    Sometimes, IGEP0046 with BASE0040 hangs during test_audio_loopback due a Kernel bug.
    test_audio_workaround_loopback avoids hang issue and test multiple times audio
    loopback to difer between Hardware error and Software error.
    
    Signed-off-by: Pau Pajuelo <ppajuelo@iseebcn.com>

--- igep_qa/tests/qaudio.py
+++ igep_qa/tests/qaudio.py
@@ -75,6 +75,63 @@ class TestAudio(unittest.TestCase):
         self.failUnless(retval[0] == 0, "failed: No DTMF found in recorded "
                         "file")
 
+    def test_audio_workaround_loopback(self):
+        """ Test Audio WORKAROUND: Loopback, sound sent to audio-out should return in audio-in
+
+        Type: Functional
+
+        Prerequisite commands:
+            - aplay
+            - arecord
+            - multimon
+
+        Prerequitsite files:
+            - dtmf.wav (on contrib/dtmf.wav)
+
+        Requirements:
+            - Audio Mini jack-Stereo loopback cable.
+
+        Description:
+            Sometimes, IGEP0046 with BASE0040 hangs during test_audio_loopback due a Kernel bug.
+            test_audio_workaround_loopback avoids hang issue and test multiple times audio
+            loopback to difer between Hardware error and Software error.
+
+            - Connect the cable between Audio IN and Audio OUT connectors.
+            - A sound with recorded DTMF tones is reproduced through Audio OUT
+              and is simultaneously recorded via Audio IN.
+            - Using demodulator multimon, it demodulates the recorded tune and
+              gets the DMTF digits.
+
+        """
+        import subprocess
+
+        # Ensure requirements are installed.
+        required = ["aplay", "arecord", "multimon"]
+        files = "/usr/igep_qa/contrib/dtmf.wav"
+        for req in required:
+            if not is_in_path(req):
+                raise Exception("Can't find %s" % req)
+        if not os.path.isfile(files):
+            raise Exception("Can't find %s" % files)
+
+        for retry in range(3):
+            commands.getoutput("rm -f /tmp/recorded.wav")
+            fd = os.open("/tmp/foo.txt", os.O_RDWR|os.O_CREAT)
+            subprocess.Popen(["aplay", "-t", "wav", "-v", "/usr/igep_qa/contrib/dtmf.wav"],
+                           stdin=None, stdout=fd, stderr=fd)
+            commands.getoutput("arecord %s -t wav -c 1 -r 8000 -f S16_LE -d 5 -v "
+                           "/tmp/recorded.wav" % self.device)
+            commands.getoutput("killall aplay")
+            os.close(fd)
+
+            retval = commands.getstatusoutput("multimon -t wav -a DTMF "
+                                  "/tmp/recorded.wav | grep 'DTMF: 5'")
+            if retval[0] == 0:
+                break
+
+        self.failUnless(retval[0] == 0, "failed: No DTMF found in recorded "
+                "file")
+
     def test_audio_playwav(self):
         """ Test Audio : Play a wav file
 
